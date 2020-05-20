9bc2813a60254f3264b415ee3a2fd6e87a1e0308
beeth0ven <beeth0vendev@gmail.com>
2020-05-20 11:03:14 +0800

    移动 UserResolver

--- src/graphql/types/User.ts
+++ src/graphql/types/User.ts
@@ -1,6 +1,4 @@
-import { UserEntity } from './../../entity/UserEntity';
-import { Context } from './../../app/context';
-import { ObjectType, ID, Field, Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import { ObjectType, ID, Field } from "type-graphql";
 
 @ObjectType()
 export class User {
@@ -15,20 +13,3 @@ export class User {
   username: string
 }
 
-
-@Resolver(User) 
-export class UserResolver {
-
-  @Mutation(returns => User) 
-  async register(
-    @Arg('username') username: string,
-    @Arg('phone') phone: string,
-    @Ctx() context: Context 
-  ): Promise<User> {
-    const userRepository = context.connection.getRepository(UserEntity);
-    await userRepository.insert({ username, phone });
-    const user = await userRepository.findOne({ username });
-    if (user == null) throw new Error('注册失败');
-    return user;
-  }
-}
