74bd3170189293bc23f136c4238d3a95797fd4a2
beeth0ven <beeth0vendev@gmail.com>
2020-05-29 17:44:10 +0800

    提升 tag 校验

--- src/functional/tag/isUserInputTag.ts
+++ src/functional/tag/isUserInputTag.ts
@@ -1,5 +1,7 @@
 
 
 export function isUserInputTag(word: string): boolean {
-  return word.startsWith('#') && !word.startsWith('#@') && word.length < 31;
+  return word.startsWith('#') 
+    && !word.includes('@')
+    && word.length < 31;
 }
