8c010a5dc72cc04173bde510942539085c8041eb
beeth0ven <beeth0vendev@gmail.com>
2020-05-29 11:53:39 +0800

    post 加入 kind 属性

--- src/entity/PostTagSumEntity.ts
+++ src/entity/PostTagSumEntity.ts
@@ -1,20 +1,32 @@
-import { Entity, ManyToOne, PrimaryColumn } from "typeorm";
+import { Entity, ManyToOne, PrimaryColumn, Column } from "typeorm";
 import { PostEntity } from "./PostEntity";
 import { TagEntity } from "./TagEntity";
 import { MetaEntity } from "../functional/entity/MetaEntity";
+
+export enum PostTagSumKind {
+  userInput = 'userInput',
+  autoDetect = 'autoDetect',
+  detectUser = 'detectUser'
+}
 
 @Entity()
 export class PostTagSumEntity extends MetaEntity {
 
   @PrimaryColumn({ type: 'varchar', length: 36 })
-  postId: string
+  postId: string;
+
+  @Column({
+    type: 'enum',
+    enum: PostTagSumKind,
+  })
+  kind: PostTagSumKind;
 
   @PrimaryColumn()
-  tagName: string
+  tagName: string;
 
   @ManyToOne(() => PostEntity, post => post.postTagSums)
-  post?: PostEntity
+  post?: PostEntity;
   @ManyToOne(() => TagEntity, tag => tag.postTagSums)
-  tag?: TagEntity
+  tag?: TagEntity;
 
 }
