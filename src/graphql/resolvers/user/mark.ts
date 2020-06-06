a48bb91967aeb0214435e620b96156dd0ea44821
beeth0ven <beeth0vendev@gmail.com>
2020-06-06 19:28:25 +0800

    统计博文标记计数

--- src/graphql/resolvers/user/mark.ts
+++ src/graphql/resolvers/user/mark.ts
@@ -1,3 +1,4 @@
+import { PostEntity } from './../../../entity/PostEntity';
 import { PostUserMarkEntity } from './../../../entity/PostUserMarkEntity';
 import { InputType, Field } from "type-graphql";
 import { AppContext } from "../../../app/context";
@@ -21,12 +22,14 @@ export async function mark(
   const connection = container.resolve(Connection);
   const postUserMarkRepository = connection.getRepository(PostUserMarkEntity);
   const userRepository = connection.getRepository(UserEntity);
+  const postEntity = connection.getRepository(PostEntity);
   const userId = getPayloadUserId(tokenPayload);
 
   const link = await postUserMarkRepository.findOne({ userId, postId });
   if (link == null) {
     await postUserMarkRepository.insert({ userId, postId });
     await userRepository.increment({ id: userId }, 'marksCount', 1);
+    await postEntity.increment({ id: postId }, 'marksCount', 1);
   }
   return 'success';
 }
--- src/graphql/resolvers/user/unmark.ts
+++ src/graphql/resolvers/user/unmark.ts
@@ -4,6 +4,7 @@ import { Connection } from "typeorm";
 import { PostUserMarkEntity } from "../../../entity/PostUserMarkEntity";
 import { getPayloadUserId } from "../../../functional/token/tokenservice";
 import { UserEntity } from "../../../entity/UserEntity";
+import { PostEntity } from "../../../entity/PostEntity";
 
 @InputType()
 export class UnmarkInput {
@@ -20,12 +21,14 @@ export async function unmark(
   const connection = container.resolve(Connection);
   const postUserMarkRepository = connection.getRepository(PostUserMarkEntity);
   const userRepository = connection.getRepository(UserEntity);
+  const postEntity = connection.getRepository(PostEntity);
   const userId = getPayloadUserId(tokenPayload);
 
   const link = await postUserMarkRepository.findOne({ userId, postId });
   if (link != null) {
     await postUserMarkRepository.delete({ userId, postId });
     await userRepository.decrement({ id: userId }, 'marksCount', 1);
+    await postEntity.decrement({ id: postId }, 'marksCount', 1);
   }
   return 'success';
 }
