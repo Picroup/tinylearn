da08d1fb3195584d83348cfbe199a0a3a4b59d8a
beeth0ven <beeth0vendev@gmail.com>
2020-06-06 21:17:02 +0800

    统计标签博文浏览数

--- src/graphql/resolvers/post/viewPost.ts
+++ src/graphql/resolvers/post/viewPost.ts
@@ -1,7 +1,9 @@
+import { TagEntity } from './../../../entity/TagEntity';
+import { PostTagSumEntity } from './../../../entity/PostTagSumEntity';
 import { PostEntity } from './../../../entity/PostEntity';
 import { InputType, Field } from "type-graphql";
 import { AppContext } from "../../../app/context";
-import { Connection } from "typeorm";
+import { Connection, In } from "typeorm";
 
 
 @InputType()
@@ -18,5 +20,16 @@ export async function viewPost(
   const connection = container.resolve(Connection);
   const postRepository = connection.getRepository(PostEntity);
   await postRepository.increment({ id: postId }, 'viewsCount', 1);
+  await incrementTagsPostsViewsCount(connection, postId);
   return 'success';
+}
+
+async function incrementTagsPostsViewsCount(connection: Connection, postId: string) {
+  const postTagSumRepository = connection.getRepository(PostTagSumEntity); 
+  const tagRepository = connection.getRepository(TagEntity); 
+
+  const links = await postTagSumRepository.find({ postId });
+  const tagNames = links.map(link => link.tagName);
+
+  await tagRepository.increment({ name: In(tagNames) }, 'postsViewsCount', 1);
 }
