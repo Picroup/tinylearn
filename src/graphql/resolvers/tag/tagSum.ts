75606434cf79174aa358d8123f46d8bb348383fb
beeth0ven <beeth0vendev@gmail.com>
2020-06-19 13:20:59 +0800

    优化 sum 返回

--- src/graphql/resolvers/tag/tagSum.ts
+++ src/graphql/resolvers/tag/tagSum.ts
@@ -1,3 +1,4 @@
+import { TagEntity } from './../../../entity/TagEntity';
 import { TagSumary } from './../../types/TagSumary';
 import { Tag } from './../../types/Tag';
 import { AppContext } from './../../../app/context';
@@ -8,6 +9,7 @@ export async function tagSum(
   { container }: AppContext,
   tag: Tag,
 ): Promise<TagSumary> {
+  if (tag instanceof TagEntity && tag.sum != null) return tag.sum;
   const dataloader = container.resolve<EntityDataLoader<string, TagSumEntity>>('EntityDataLoader<string, TagSumEntity>');
   const sum = await dataloader.load(tag.name);
   if (sum == null) throw new Error(`no tag sum with tag name: ${tag.name}`);
--- src/graphql/resolvers/user/userSum.ts
+++ src/graphql/resolvers/user/userSum.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from './../../../entity/UserEntity';
 import { UserSumary } from '../../types/UserSumary';
 import { AppContext } from './../../../app/context';
 import { User } from './../../types/User';
@@ -8,6 +9,7 @@ export async function userSum(
   { container }: AppContext,
   user: User,
 ): Promise<UserSumary> {
+  if (user instanceof UserEntity && user.sum != null) return user.sum;
   const dataloader = container.resolve<EntityDataLoader<string, UserSumEntity>>('EntityDataLoader<string, UserSumEntity>');
   const sum = await dataloader.load(user.id);
   if (sum == null) throw new Error(`no user sum with userId: ${user.id}`);
